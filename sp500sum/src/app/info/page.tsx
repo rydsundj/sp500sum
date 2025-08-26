362d1aff757df33c9b8b94f7630577171b8fbf0c
rydsundj <oliverrydsundjones@gmail.com>
2025-08-26 11:33:28 +0200

    redo

---
+++ sp500sum/src/app/info/page.tsx
@@ -0,0 +1,13 @@
+
+ 
+export default async function Page() {
+  const posts = await getPosts()
+ 
+  return (
+    <ul>
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </ul>
+  )
+}
---
+++ sp500sum/src/app/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+
+import { useState } from "react";
+
+export default function Home() {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (e: React.FormEvent) => 
+  {
+    e.preventDefault();
+    console.log("Searching for:", query);
+    //call api
+  };
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+          <h1 className="font-mono mb-2 tracking-[-.01em]">
+            Enter a ticker, for example{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+              GME
+            </code>
+            .
+          </h1>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <form onSubmit={handleSearch} className="flex items-center gap-2">
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            className="border rounded-lg p-2 w-40"
+          />
+
+          <button className="w-32 h-12 rounded bg-green-900 text-white relative overflow-hidden group z-10 hover:text-white duration-1000">
+            <span className="absolute bg-emerald-600 w-36 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all">
+
+            </span>
+            <span className="absolute bg-emerald-800 w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all">
+
+            </span>
+              Search
+          </button>
+
+          </form>
+
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://rydsundj.github.io/rydsundj/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          made by rydsundj →
+        </a>
+      </footer>
+    </div>
+  );
+}
