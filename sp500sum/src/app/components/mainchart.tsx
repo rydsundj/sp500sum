e7a9e2a2baf85624d199bfbbd440291555dbd6c5
rydsundj <oliverrydsundjones@gmail.com>
2025-09-07 18:30:41 +0200

    edit

--- sp500sum/src/app/components/mainchart.tsx
+++ sp500sum/src/app/components/mainchart.tsx
@@ -14,7 +14,6 @@ export default function MainChart({ ticker }: { ticker: string }) {
   async function showInitialChart() {
     const res = await fetch(`http://127.0.0.1:8000/ticker/${ticker}/chart`);
     const chartData = await res.json();
-    console.log(chartData);
     setData(chartData);
   }
 
